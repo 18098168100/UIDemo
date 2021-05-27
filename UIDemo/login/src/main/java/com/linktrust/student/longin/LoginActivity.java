3b443dc7e17ac636e95ca5d3f943b18c98adbd6e
haohai <luruiqian@gomeplus.com>
2021-05-27 15:48:34 +0800

    gradle 编程

--- UIDemo/login/src/main/java/com/linktrust/student/longin/LoginActivity.java
+++ UIDemo/login/src/main/java/com/linktrust/student/longin/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.linktrust.student.longin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
